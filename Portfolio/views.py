9f3704b4ff0b9041387ac70a926a7a644cfc41f6
Manav-arora-87 <aroramanav887@gmail.com>
2022-05-22 16:46:22 +0530

    'first1'

--- Portfolio/views.py
+++
@@ -1,9 +0,0 @@
-import json
-from django.shortcuts import redirect, render
-from django.http import JsonResponse
-from django.http import HttpResponseRedirect
-
-# Create your views here.
-import os
-def index(request):
-    return render(request,"index.html")
